02cd2919948212695b765225fd0fd2a2502773c8
Ruth <ruthkyalo21@gmail.com>
2025-08-30 16:55:25 +0300

    feat: add pagination, filtering, search, and ordering to Books API

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 from books.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -30,6 +31,7 @@ class BookViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
     # Search & ordering functionality
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'author']  # Search by title or author
     ordering_fields = ['rating', 'created_at']  # Allow sorting by rating or creation date
+    filterset_fields = ['rating']  # new → filter by rating
