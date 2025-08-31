9af5c84d0cce4a1df61f62b99a9d564966a03592
Ruth <ruthkyalo21@gmail.com>
2025-08-31 16:58:02 +0300

    add PythonAnywhere domain in setting.py file

--- bookreview/settings.py
+++ bookreview/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 DEBUG = True
 
 # Allow localhost + PythonAnywhere domain
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "<ruth1>.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "ruth1.pythonanywhere.com"]
 
 
 # Application definition
